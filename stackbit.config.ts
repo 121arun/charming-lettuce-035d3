ad6fba93168c6057c5d2ba2868c2ba012414f2bc
Stackbit Code Editor <projects@stackbit.com>
2023-07-02 06:44:47 +0000

    stackbit.config.ts: changed

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,35 +1,31 @@
-import { defineStackbitConfig } from '@stackbit/types'
-import { GitContentSource } from '@stackbit/cms-git'
-import { button } from './.stackbit/models/button'
-import { hero } from './.stackbit/models/hero'
-import { page } from './.stackbit/models/page'
-import { stats } from './.stackbit/models/stats'
+import { GitContentSource } from '@stackbit/cms-git';
+import { defineStackbitConfig } from '@stackbit/types';
+import { allModels } from './.stackbit/models';
 
-export default defineStackbitConfig({
-  stackbitVersion: '~0.6.0',
-  ssgName: 'nextjs',
-  nodeVersion: '16',
-  contentSources: [
-    new GitContentSource({
-      rootPath: __dirname,
-      contentDirs: ['content'],
-      models: [button, hero, page, stats],
-      assetsConfig: {
+const gitContentSource = new GitContentSource({
+    rootPath: __dirname,
+    contentDirs: ['content'],
+    models: Object.values(allModels),
+    assetsConfig: {
         referenceType: 'static',
         staticDir: 'public',
         uploadDir: 'images',
         publicPath: '/'
-      }
-    }),
-  ],
-  sidebarButtons: [
-    {
-      type: 'model',
-      label: 'Global styles',
-      icon: 'tools',
-      modelName: 'GlobalStylesConfig',
-    },
+    }
+});
 
-  ],
-})
+export const sbConfig = defineStackbitConfig({
+    stackbitVersion: '~0.6.0',
+    ssgName: 'nextjs',
+    nodeVersion: '16',
+    contentSources: [gitContentSource],
+    presetSource: {
+        type: 'files',
+        presetDirs: ['.stackbit/presets']
+    },
+    styleObjectModelName: 'ThemeStyle'
+});
+
+export default sbConfig;
+
 
